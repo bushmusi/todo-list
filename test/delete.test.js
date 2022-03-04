b0e11398cd1cf58968a5e7ebb6cfd2dcf6551ef9
bushmusi <bushera.mestofa@gmail.com>
2022-03-04 09:50:43 +0300

    Update delete item value

--- test/delete.test.js
+++ test/delete.test.js
@@ -1,16 +1,24 @@
 import deleteLi from '../src/components/delete-val.js';
+import domElement from '../src/components/add-val.js';
 
 describe('This test is for deleting an item', () => {
   test('testing an item should minimise the amount', () => {
     document.body.innerHTML = `<div>
-      <ul id="list-ul">
+      <ul id="list-cont">
         <li id='li-1'>Call to GF</li>
       </ul>
     </div>`;
     const id = 1;
+    const obj = [{
+      description: 'Call to GF',
+      completed: false,
+      index: 1,
+    }];
+    localStorage.setItem('todo-list', JSON.stringify(obj));
+    deleteLi(id);
     const liItem = document.getElementById(`li-${id}`);
     liItem.remove();
-    const list = document.querySelectorAll('#list-ul li');
+    const list = document.querySelectorAll('#list-cont #li-1');
     expect(list).toHaveLength(0);
   });
 });
